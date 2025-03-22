060d56a346a9cf4273355a855c6464899a059928
Pradianta <shabilun810@gmail.com>
2025-03-23 03:04:41 +0700

    Menambahkan file baru

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Tugas4 from './src/components/Tugas4.vue'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
